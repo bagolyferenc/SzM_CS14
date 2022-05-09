b1fe42cfb69f41dea9bda3fbe40fc2f59582f528
Levente Varadi <lvaradi@Leventes-MacBook-Pro.local>
2022-05-09 21:16:05 +0200

    unit test

---
+++ Project-core/src/main/java/hu/unideb/inf/TAJ.java
@@ -0,0 +1,140 @@
+package hu.unideb.inf;
+
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+
+public class TAJ {
+    private IntegerProperty id = new SimpleIntegerProperty(this,"id");
+    private StringProperty tajszam = new SimpleStringProperty(this, "tajszam");
+    private StringProperty name = new SimpleStringProperty(this, "name");
+    //private GenderType gender;
+    private StringProperty vercsoport = new SimpleStringProperty(this, "vercsoport");
+    private StringProperty lakcim = new SimpleStringProperty(this, "lakcim");
+    private StringProperty szhely = new SimpleStringProperty(this, "szhely");
+    private StringProperty anev = new SimpleStringProperty(this, "anev");
+    private ObjectProperty<LocalDate> sznap = new SimpleObjectProperty<>(this, "sznap");
+    private ObjectProperty<ObservableList<Oltas>> oltasok = new SimpleObjectProperty<>(this, "oltasok");
+
+
+    public ObservableList<Oltas> getOltasok() {
+        return oltasok.get();
+    }
+
+    public ObjectProperty<ObservableList<Oltas>> oltasokProperty() {
+        return oltasok;
+    }
+
+    public void setOltasok(ObservableList<Oltas> oltasok) {
+        this.oltasok.set(oltasok);
+    }
+
+    /*public enum GenderType{
+        MALE,FEMALE,NA
+    }*/
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getTajszam() {
+        return tajszam.get();
+    }
+
+    public StringProperty tajszamProperty() {
+        return tajszam;
+    }
+
+    public void setTajszam(String tajszam) {
+        this.tajszam.set(tajszam);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getVercsoport() {
+        return vercsoport.get();
+    }
+
+    public StringProperty vercsoportProperty() {
+        return vercsoport;
+    }
+
+    public void setVercsoport(String vercsoport) {
+        this.vercsoport.set(vercsoport);
+    }
+
+    public String getLakcim() {
+        return lakcim.get();
+    }
+
+    public StringProperty lakcimProperty() {
+        return lakcim;
+    }
+
+    public void setLakcim(String lakcim) {
+        this.lakcim.set(lakcim);
+    }
+
+    public String getSzhely() {
+        return szhely.get();
+    }
+
+    public StringProperty szhelyProperty() {
+        return szhely;
+    }
+
+    public void setSzhely(String szhely) {
+        this.szhely.set(szhely);
+    }
+
+    public String getAnev() {
+        return anev.get();
+    }
+
+    public StringProperty anevProperty() {
+        return anev;
+    }
+
+    public void setAnev(String anev) {
+        this.anev.set(anev);
+    }
+
+    public LocalDate getSznap() {
+        return sznap.get();
+    }
+
+    public ObjectProperty<LocalDate> sznapProperty() {
+        return sznap;
+    }
+
+    public void setSznap(LocalDate sznap) {
+        this.sznap.set(sznap);
+    }
+
+    /*public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }*/
+}
---
+++ Project-desktop/src/main/java/hu/unideb/inf/App.java
@@ -0,0 +1,53 @@
+package hu.unideb.inf;
+
+import hu.unideb.dao.TAJDAOImplement;
+import hu.unideb.dao.TajDAO;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+
+    private static Stage stage;
+
+    @Override
+    public void start(Stage stage) {
+
+        App.stage = stage;
+        App.loadFXML("/fxml/main_window.fxml");
+
+        stage.show();
+    }
+
+    public static FXMLLoader loadFXML(String fxml){
+        FXMLLoader Loader = new FXMLLoader(App.class.getResource(fxml));
+        Scene scene = null;
+        try {
+           Parent root = Loader.load();
+           scene = new Scene(root);
+           scene.getStylesheets().add("/css/style.css");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        return Loader;
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
